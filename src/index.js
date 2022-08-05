fcf5b622acb8d4396fcae45060c388bae3612ef2
c0c0pang <coc1789@naver.com>
2022-08-06 00:51:19 +0900

    리액트를 이용한 익명 게시판 제작

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+//만들어진 컴포넌트를 껍데기에 넣는 역할
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
